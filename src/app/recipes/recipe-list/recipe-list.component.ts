cdda1ac97f41a54559d243904696bb9bf493c90f
RamTheeru <theertha808@gmail.com>
2019-04-05 17:07:13 +0530

    5 files modified, 6 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe } from '../recipe';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes:Recipe[] =[
 
-  constructor() { }
+]; 
+recipe : Recipe={
+name: 'A test recipe',
+description:'This is a simple test.',
+imagePath :'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg' 
+}
+rcp : Recipe={
+name: 'A test recipe',
+description:'This is a simple test.',
+imagePath :'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg' 
+}
+
+  constructor() {
+    this.recipes.push(this.recipe);
+     this.recipes.push(this.rcp);
+   }
 
   ngOnInit() {
   }
