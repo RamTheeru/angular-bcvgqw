0d572aeb2e52b457e9cb0ddfb6b952c933e6b3ae
RamTheeru <theertha808@gmail.com>
2019-04-18 18:19:54 +0530

    2 files modified

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit,Input,OnChanges,SimpleChanges } from '@angular/core';
+import { Component, OnInit,Input,OnChanges,SimpleChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit,OnChanges {
+export class ServerElementComponent implements OnInit,OnChanges,DoCheck,AfterContentInit,AfterContentChecked {
 @Input('srvElement')  element : {type:string,name : string,content :string};
 @Input() name:string;
+@ViewChild('header') header : ElementRef;
   constructor() {
     console.log('construct  called !!');
    }
 
   ngOnInit() {
     console.log('ngoninit  called !!');
+    //console.log('Text Content '+this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked(){
+console.log('ngAfterViewChecked  called !!');
+  }
+  ngAfterViewInit(){
+console.log('ngAfterViewInit  called !!');
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit  called !!');
+  }
+  ngDoCheck(){
+console.log('ngDoCheck  called !!');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked  called !!');
   }
   ngOnChanges(changes : SimpleChanges){
 console.log('ngOnchanges  called !!');
