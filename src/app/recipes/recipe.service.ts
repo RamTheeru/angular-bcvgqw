78bc3d1982d1a9cfd00bdff5c3df3f47a9977149
RamTheeru <theertha808@gmail.com>
2019-05-14 13:29:54 +0530

    1 file modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,17 +7,17 @@ export class RecipeService {
  recipeSelected = new EventEmitter<Recipe>();
 private recipes:Recipe[] =[ 
   new Recipe ( 
-    'Tasty Scnitzel',
+    'Tasty Schnitzel',
     'A super tasty Schnitzel - just awesome !!!',
-    'https://upload.wikimedia.org/commons/7/72/Schnitzel.JPG',
+    'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
     [
       new Ingredient('Meat',1),
       new Ingredient('Frence Fries',20)
     ]),
     new Recipe ( 
-    'big Fat burger',
+    'Big Fat burger',
     'what else you need to say???',
-    'https://upload.wikimedia.org/commons/b/be/Burger_King_Angus_bacon_%26_Cheese_Steak_Burger.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
     [
       new Ingredient('Buns',2),
       new Ingredient('Meat',1)
