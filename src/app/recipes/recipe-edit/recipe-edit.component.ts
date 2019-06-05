5068eb4ea161a7ccfc8f0785c93ca530072c727c
RamTheeru <theertha808@gmail.com>
2019-06-05 20:20:01 +0530

    1 file modified

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute,Params} from '@angular/router';
-
+import {FormGroup,FormControl} from '@angular/forms';
+import {RecipeService} from '../recipe.service';
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
@@ -9,15 +10,36 @@ import {ActivatedRoute,Params} from '@angular/router';
 export class RecipeEditComponent implements OnInit {
  id : number;
  editMode = false;
-  constructor(private route : ActivatedRoute) { }
+ recipeForm : FormGroup;
+  constructor(private route : ActivatedRoute,private recpServ : RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params:Params)=>{
             this.id=+params['id'];
             this.editMode = params['id'] != null;
+            this.initForm();
       }
     )
+  }
+  private initForm(){
+   
+    let recipeName='';
+    let recipeImgPath = '';
+   let recpDesc = '';
+    if(this.editMode)
+    {
+       const recp = this.recpServ.getRecipeById(this.id);
+      recipeName = recp.name;
+      recipeImgPath=recp.imagePath;
+      recpDesc=recp.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name':new FormControl(recipeName),
+      'imagePath':new FormControl(recipeImgPath),
+      'description':new FormControl(recpDesc)
+    })
+
   }
 
 }
