6b37c863779d80678e759994d67313ad9fdd9b56
RamTheeru <theertha808@gmail.com>
2019-06-05 21:03:49 +0530

    2 files modified

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute,Params} from '@angular/router';
-import {FormGroup,FormControl} from '@angular/forms';
+import {FormGroup,FormControl,FormArray} from '@angular/forms';
 import {RecipeService} from '../recipe.service';
 @Component({
   selector: 'app-recipe-edit',
@@ -27,22 +27,34 @@ export class RecipeEditComponent implements OnInit {
     let recipeName='';
     let recipeImgPath = '';
    let recpDesc = '';
+   let recpIngredients =  new FormArray([]);
     if(this.editMode)
     {
        const recp = this.recpServ.getRecipeById(this.id);
       recipeName = recp.name;
       recipeImgPath=recp.imagePath;
       recpDesc=recp.description;
+      if(recp.ingredients.length>0)
+      {
+        for(let ing of recp.ingredients)
+        {
+          recpIngredients.push(new FormGroup({
+              'name':new FormControl(ing.name),
+              'amount':new FormControl(ing.amount)
+          }))
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name':new FormControl(recipeName),
       'imagePath':new FormControl(recipeImgPath),
-      'description':new FormControl(recpDesc)
+      'description':new FormControl(recpDesc),
+      'ingredients':recpIngredients
     })
 
   }
   onSubmit(){
-    console.log(this.recipeFormr);
+    console.log(this.recipeForm);
   }
 
 }
