359547d6299915681d18648080557790369622ae
RamTheeru <theertha808@gmail.com>
2019-06-24 20:34:35 +0530

    3 files modified

--- src/app/hero-service.service.ts
+++ src/app/hero-service.service.ts
@@ -7,7 +7,7 @@ singleHero : Hero;
  HEROES: Hero[] = [
   { id: 1, name: 'Robert Downey.Jr',
   desc : 
-  `Anthony Edward "Tony" Stark is a character portrayed by Robert Downey Jr. in the Marvel Cinematic Universe (MCU) film franchise, based on the Marvel Comics character of the same name and known commonly by his alter ego, Iron Man. In the films, Stark is an industrialist, genius inventor, hero and former playboy who is CEO of Stark Industries. At the beginning of the series, he is a chief weapons manufacturer for the U.S. military, until he has a change of heart and redirects his technical knowledge into the creation of powered exoskeleton suits of armor which he uses to defend against those that would threaten peace around the world.` ,
+  `Anthony Edward "Tony" Stark is a character portrayed by Robert Downey Jr. in the Marvel Cinematic Universe (MCU) film franchise, based on the Marvel Comics character of the same name and known commonly by his alter ego, Iron Man.` ,
  imgpath : 'https://media3.mensxp.com/media/content/2018/Apr/tony-stark-quotes-proving-hes-the-coolest-avenger-1400x653-1522835488_1100x513.jpg'}
   // { id: 12, name: 'Narco' },
   // { id: 13, name: 'Bombasto' },
