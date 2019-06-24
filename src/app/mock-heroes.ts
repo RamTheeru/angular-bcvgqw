0c62414bfd6fde799acff0edcab57f8665a22bc4
RamTheeru <theertha808@gmail.com>
2019-06-24 18:50:38 +0530

    3 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,9 +1,7 @@
  import { Hero } from './hero';
 
  export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice',desc : 'Anthony Edward "Tony" Stark is a character portrayed by Robert Downey Jr. in the Marvel Cinematic Universe (MCU) film franchise, based on the Marvel Comics character of the same name and known commonly by his alter ego, Iron Man. In the films, Stark is an industrialist, genius inventor, hero and former playboy who is CEO of Stark Industries. At the beginning of the series, he is a chief weapons manufacturer for the U.S. military, until he has a change of heart and redirects his technical knowledge into the creation of powered exoskeleton suits of armor which he uses to defend against those that would threaten peace around the world. '+
-''+
-'As of 2019, the character was one of the central figures of the MCU, having appeared in ten films of the series;[N 1] from his introduction in Iron Man, up to Avengers: Endgame. In 2015, the evolution of the character over the series was described as "the defining arc of the Marvel Cinematic Universe".[1] In 2018, it was argued that "no other character goes on as transformative a journey as Iron Man".[2]' }
+  { id: 11, name: 'Mr. Nice',desc : 'Anthony Edward "Tony" Stark is a character portrayed by Robert Downey Jr. in the Marvel Cinematic Universe (MCU) film franchise, based on the Marvel Comics character of the same name and known commonly by his alter ego, Iron Man. In the films, Stark is an industrialist, genius inventor, hero and former playboy who is CEO of Stark Industries. At the beginning of the series, he is a chief weapons manufacturer for the U.S. military, until he has a change of heart and redirects his technical knowledge into the creation of powered exoskeleton suits of armor which he uses to defend against those that would threaten peace around the world.'}
   // { id: 12, name: 'Narco' },
   // { id: 13, name: 'Bombasto' },
   // { id: 14, name: 'Celeritas' },
